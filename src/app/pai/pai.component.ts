de1d0d48c41cd5c90f966648c207213055d38c8f
Mauricio Afonso <mafonso@cleartech.com.br>
2020-01-28 17:34:10 -0300

    wip

--- src/app/pai/pai.component.ts
+++ src/app/pai/pai.component.ts
@@ -9,18 +9,18 @@ import { TarefaService } from '../tarefa.service';
 })
 export class PaiComponent {
 
-  tarefaList = ['Limpar o quarto', 'Estudar Angular', 'Hora de brincar'];
+  tarefas = ['Limpar o quarto', 'Estudar Angular', 'Hora de brincar'];
   pendencias = [];
 
   constructor(private tarefaService: TarefaService) {
     tarefaService.concluido$.subscribe(tarefa => {
       this.pendencias = this.pendencias.filter(t => t !== tarefa);
-      this.tarefaList.push(tarefa);
+      this.tarefas.push(tarefa);
     });
   }
 
   criarTarefa() {
-    const tarefa = this.tarefaList.shift();
+    const tarefa = this.tarefas.shift();
     this.pendencias.push(tarefa);
     this.tarefaService.criar(tarefa);
   }
